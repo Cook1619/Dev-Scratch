ef5f98a92321539c30e09539bbbaa00089528bb9
Cook1619 <cook6348@gmail.com>
2023-01-05 16:49:29 -0600

    big o of objects and arrays

---
+++ array-object-big-o.js
@@ -0,0 +1,25 @@
+const person = {
+    name: 'Ned',
+    age: 122,
+    isEmployed: true
+}
+
+// ! Big O of Objects
+// * Insertion O(1)
+// * Removal O(1)
+// * Searching O(n)
+// * Access O(1)
+
+// * Object.keys O(n)
+// * Object.values O(n)
+// * Object.entries O(n)
+// * hasOwnProperty O(1)
+
+// Ordered list unlike objects
+let names = ["Bill", "Ted", "Phil"];
+
+// ! Big O of Arrays
+// * Insertion depends..
+// * Removal depends..
+// * Searching O(n)
+// * Access O(1)
