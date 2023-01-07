64de7cedec533b75971aa0dabade3bed4908004b
Matt Cook <mathew.cook@mastery.net>
2023-01-06 18:57:08 -0600

    Notes on breaking down problems

---
+++ problem-solving.js
@@ -0,0 +1,38 @@
+// ! Problem solving breakdown
+/*
+  Understand the Problem
+  ? Can you restate the problem in your own words?
+  ? What are the inputs that go into the problem?
+  ? What are the outputs? What should it look like?
+  ? Can the outputs be determined from the inputs?  You may not be able to answer this immediately 
+  ? How should I label the important pieces of data that are part of the problem?
+*/
+
+/*
+  Exploring Examples
+  ? Start with simple examples
+  ? Progress to more complex examples
+  ? Explore examples with empty inputs
+  ? Explore examples with invalid inputs
+*/
+
+/*
+  Break it down!
+  ? Write down the steps you wanna take, could be small little notes for the path you wanna take
+  ? Write the skeleton of your function and write pseudo code steps to take in there
+*/
+
+// ! Sample
+// TODO: Write a function which takes in a string and returns counts of each character in a string
+/*
+    Return an object, only care about alpha numeric characters, ignore spaces
+*/
+
+function charCount(str) {
+  // make an object to return
+  // loop over string, for each character
+  // if the char is a num/letter and is a key in object, add one to count
+  // if the char is a num/letter and is NOT a key in object, add it and set value to one
+  // if char is something else (space, period, etc) don't do anything
+  // return object
+}
