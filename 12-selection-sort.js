5e72cd267ec0aded340dbde4969ea46518ba7283
Matt Cook <mathew.cook@mastery.net>
2023-01-12 16:17:24 -0600

    insertion sort p1

--- 12-selection-sort.js
+++ 12-selection-sort.js
@@ -9,6 +9,7 @@
   - If a smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array.
   - If the minimum is not the value (index) you initially began with, swap those two values
   - Repeat this with the next element until the array is sorted
+  - O(n^2)
  */
 function selectionSort(arr) {
   const swap = (arr, index1, index2) => {
---
+++ 13-insertion-sort.js
@@ -0,0 +1,12 @@
+// ! Insertion Sort
+/*
+  - Builds up the sort by gradually creating a larger portion which is always sorted
+  - It iterates over the array and puts the value where it needs to of
+*/
+
+/* Example
+ [2]
+ Is 5 greater then 2? shifts it over one
+ [2,5,7,4]
+ [2,5,4,7]
+*/
