a05a0ee21b5ff3a073061fd03b8afa3907ee8ca9
Mathew Cook <cook6348@gmail.com>
2023-07-05 20:01:33 -0500

    fem js hard parts

---
+++ JS-Hard-Parts/index.js
@@ -0,0 +1,8 @@
+// declaring a variable called num and assigning 3 to it
+const num = 3;
+
+// declaring a function with one input, then declares a variable named result which is the input times 2, then returning result
+function multiplyBy2(input){
+    const result = input * 2;
+    return result;
+}
