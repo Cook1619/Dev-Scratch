34fa2831cdf57f3f287261782243fed7e158dc31
Mathew Cook <cook6348@gmail.com>
2023-07-06 08:05:46 -0500

    writing servers in vim

---
+++ Full-Stack-FEM/simpleServer.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'content-type': 'text/html'});
+  fs.createReadStream('index.html'.pipe(res));
+});
+
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
+
+
+
+
+
+
