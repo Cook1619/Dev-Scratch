6b68a1f1b035702506743954fd679d20c5e49212
Mathew Cook <cook6348@gmail.com>
2023-07-11 19:16:55 -0500

    more generics

--- Typescript/demo.tsx
+++ Typescript/demo.tsx
@@ -19,3 +19,28 @@ function merge<T, U>(obj1 : T, obj2: U): T & U{
 }
 
 const comboObject = merge({name: 'Matt'}, {pets:['Flynn']});
+
+// Adding Type constraints  generics
+
+function mergeNumber2<T extends object, U extends object>(obj1: T, obj2: U) : T & U {
+    return {
+        ...obj1,
+        ...obj2
+    }
+}
+// works
+mergeNumber2({name: 'test'}, {married: true})
+
+// doesn't
+// mergeNumber2({name: 'test2'}, 5)
+
+interface Length {
+    length: number
+}
+
+// sense the interface follows the rules of what's being return aka length is a number, TS is happy
+function getLength<T extends Length>(ele: T): number {
+    return ele.length * 2;
+}
+
+getLength("test")
