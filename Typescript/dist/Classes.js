9a61da4462007cd7775ab5e096ef990780219dcf
Mathew Cook <cook6348@gmail.com>
2023-07-14 14:10:25 -0500

    Playing around with classes

--- Typescript/dist/Classes.js
+++ Typescript/dist/Classes.js
@@ -1,1 +1,16 @@
 "use strict";
+class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+        console.log('in constructor');
+    }
+    taunt() {
+        console.log('Booooyah!');
+    }
+}
+const player1 = new Player("Flynn", "Cook");
+player1.taunt();
+console.log(player1.first);
+const player2 = new Player("Kali", "Cook");
+player2.taunt();
---
+++ Typescript/src/Classes.ts
@@ -0,0 +1,19 @@
+class Player {
+    first: string;
+    last: string;
+    constructor(first: string, last: string) {
+        this.first = first;
+        this.last = last;
+        console.log('in constructor')
+    }
+    taunt(){
+        console.log('Booooyah!')
+    }
+}
+
+const player1 = new Player("Flynn", "Cook");
+player1.taunt();
+console.log(player1.first)
+
+const player2 = new Player("Kali", "Cook");
+player2.taunt();
