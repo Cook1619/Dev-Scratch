aca37df548374239d939d49205072d7910b02d17
Mathew Cook <cook6348@gmail.com>
2023-07-14 14:16:30 -0500

    private fields cant be accessed outside the class itself

--- Typescript/dist/Classes.js
+++ Typescript/dist/Classes.js
@@ -1,16 +1,22 @@
 "use strict";
 class Player {
     constructor(first, last) {
+        this.score = 0;
+        this.numLives = 10;
         this.first = first;
         this.last = last;
-        console.log('in constructor');
     }
     taunt() {
         console.log('Booooyah!');
+    }
+    loseLife() {
+        this.numLives -= 1;
     }
 }
 const player1 = new Player("Flynn", "Cook");
 player1.taunt();
 console.log(player1.first);
+player1.loseLife();
+console.log(player1);
 const player2 = new Player("Kali", "Cook");
 player2.taunt();
--- Typescript/src/Classes.ts
+++ Typescript/src/Classes.ts
@@ -1,19 +1,25 @@
 class Player {
     first: string;
     last: string;
+    private score = 0;
+    numLives = 10;
     constructor(first: string, last: string) {
         this.first = first;
         this.last = last;
-        console.log('in constructor')
     }
     taunt(){
         console.log('Booooyah!')
+    }
+    loseLife(){
+        this.numLives -= 1;
     }
 }
 
 const player1 = new Player("Flynn", "Cook");
 player1.taunt();
 console.log(player1.first)
+player1.loseLife()
+console.log(player1);
 
 const player2 = new Player("Kali", "Cook");
 player2.taunt();
